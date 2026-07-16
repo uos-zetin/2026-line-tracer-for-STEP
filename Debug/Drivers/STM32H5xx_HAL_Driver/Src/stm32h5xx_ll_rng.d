6b2ccc2d530123e37c7625849180d604c2eed17a
Kim Tae Hwan <kth5931@gmail.com>
2026-07-16 22:39:08 +0900

    Update menu.c

---
+++ Debug/Drivers/STM32H5xx_HAL_Driver/Src/stm32h5xx_ll_rng.d
@@ -0,0 +1,2 @@
+Drivers/STM32H5xx_HAL_Driver/Src/stm32h5xx_ll_rng.o: \
+ ../Drivers/STM32H5xx_HAL_Driver/Src/stm32h5xx_ll_rng.c
--- Main/Src/menu.c
+++ Main/Src/menu.c
@@ -17,26 +17,29 @@ typedef struct {
 	void (*func)(void);
 } Menu_TypeDef;
 
-Menu_TypeDef MENU = {
+Menu_TypeDef MENU[] = {
 
 };
 
-void Main_Menu(){
-    static uint8_t init_done = 0;
+void Main_Menu() {
+	static uint8_t init_done = 0;
 
-    // 1. 초기 1회만 실행되는 화면 그리기
-    if (!init_done) {
-        // 실제 사용 중인 LCD 타입에 맞게 설정 (ST7735 또는 ST7789)
-        Custom_LCD_Init(LCD_TYPE_ST7735); 
-        Custom_LCD_Clear();
-        
-        init_done = 1;
-    }
+	// 1. 초기 1회만 실행되는 화면 그리기
+	if (!init_done) {
+		// 실제 사용 중인 LCD 타입에 맞게 설정 (ST7735 또는 ST7789)
+		Custom_LCD_Init(LCD_TYPE_ST7735);
+		Custom_LCD_Printf("Hello World");
+		HAL_Delay(500);
+		Custom_LCD_Clear();
 
-    // 2. 메인 메뉴 로직 구현
-    UserInput_t btn_input = Button_Get_Input();
+		init_done = 1;
+	}
 
-    if (btn_input != INPUT_CMD_NONE) {
-        // 버튼 입력에 따른 처리 로직 작성
-    }
+	// 2. 메인 메뉴 로직 구현
+	UserInput_t btn_input = Button_Get_Input();
+
+	if (btn_input != INPUT_CMD_NONE) {
+		// 버튼 입력에 따른 처리 로직 작성
+
+	}
 }
